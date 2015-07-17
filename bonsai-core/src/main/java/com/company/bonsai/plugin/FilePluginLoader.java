e634920c2736db9168deacbd5b5353d79528d2dc
Stepan Hashchyshyn <stepan_hashchyshyn@epam.com>
2015-07-17 17:01:15 +0300

    Add automatic plugin resolver

--- bonsai-core/src/main/java/com/company/bonsai/plugin/FilePluginLoader.java
+++ bonsai-core/src/main/java/com/company/bonsai/plugin/FilePluginLoader.java
@@ -2,8 +2,14 @@ package com.company.bonsai.plugin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.type.filter.AssignableTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -45,54 +51,17 @@ public class FilePluginLoader {
             try {
                 URL jarURL = pluginJarFile.toURI().toURL();
                 JarClassLoader classLoader = new JarClassLoader(jarURL);
-                //resolve plugin class here
-                Class clazz = classLoader.loadClass("com.company.bonsai.plugin.libs.LibsPlugin");
+                Class clazz = classLoader.loadClass(classLoader.getMainClassName());
                 pluginClasses.add(clazz);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         return pluginClasses;
     }
-
-//    public Collection<Plugin> loadPlugins() {
-//        loadPluginsClasses(getPluginJars());
-//
-//
-//        return null;
-//
-//    }
-//
-//    private Collection<File> getPluginJars() { //maybe need to separate to JarLoader may be useful for loading script's jars
-//        File pluginDir = new File(PATH_TO_PLUGINS);
-//        File[] files = pluginDir.listFiles(file -> file.isFile() && file.getName().endsWith(".jar"));
-//        return Arrays.asList(files);
-//    }
-//
-//    private Collection<Class>  loadPluginsClasses(Collection<File> jars) {
-//        Collection<Class> pluginClasses = new ArrayList<>();
-//
-//        for (File jarFile : jars) {
-//            try {
-//                URL jarURL = jarFile.toURI().toURL();
-//                JarClassLoader classLoader = new JarClassLoader(jarURL);
-//                //resolve plugin class here
-//                Class clazz = classLoader.loadClass("com.juriy.hello.HelloPlugin");
-//                pluginClasses.add(clazz);
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            } catch (ClassNotFoundException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        return pluginClasses;
-//    }
-//
-//    private Collection<Plugin> createPlugins(Collection<Class> pluginClasses) {
-//        return null;
-//    }
 
 }
